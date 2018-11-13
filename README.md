3990ee9cdbe557c366e43dc6485cca59f3ddbdc9
ktht <karlehataht@gmail.com>
2018-11-13 23:08:58 +0200

    Add files generated w/ MadGraph
    Not sure yet if it all compiles.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hh-bbww-mem
-repository for matrix element method (MEM) code for HH->bbWW analysis
+Matrix element method (MEM) for HH->bbWW analysis
+
+## Generating the MG files
+
+1. download & unpack Madgrapg version 2.3.3: https://launchpad.net/mg5amcnlo/2.0/2.3.x/+download/MG5_aMC_v2.3.3.tar.gz
+2. download UFO files: https://cms-project-generators.web.cern.ch/cms-project-generators/BSM_gg_hh.tar
+3. unpack the UFO files to `MG5_aMC_v2_3_3/models/BSM_gg_hh`
+4. in `./MG5_aMC_v2_3_3/bin/mg5_aMC` run:
+```
+generate g g > t t~, ( t > w+ b, w+ > l+ vl ), ( t~ > w- b~, w- > l- vl~ )
+output standalone ttbar
+output standalone_cpp ttbar
+display diagrams
+
+import BSM_gg_hh
+generate g g > h h, ( h > b b~ ), ( h > w+ w-, w+ > l+ vl, w- > l- vl~ )
+output standalone hh_bbww
+output standalone_cpp hh_bbww
+display diagrams
+```
+
+## Building instructions
+
+```bash
+git clone https://github.com/HEP-KBFI/hh-bbww-mem.git $CMSSW_BASE/src/hhAnalysis/bbwwMEM
+```
+
+For dependencies, see: https://github.com/HEP-KBFI/tth-analyze-mem#building-instructions
+
---
+++ interface/mg5/me/mg5_ttbar2WbWb_WW2lvlv.h
@@ -0,0 +1,157 @@
+//==========================================================================
+// This file has been automatically generated for C++ Standalone by
+// MadGraph5_aMC@NLO v. 2.3.3, 2015-10-25
+// By the MadGraph5_aMC@NLO Development Team
+// Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
+//==========================================================================
+
+#ifndef MG5_sm_ttbar2WbWb_WW2lvlv_H
+#define MG5_sm_ttbar2WbWb_WW2lvlv_H
+
+#include <complex> 
+#include <vector> 
+
+#include "hhAnalysis/bbwwMEM/interface/mg5/parameters/Parameters_sm_ttbar2WbWb_WW2lvlv.h"
+
+//==========================================================================
+// A class for calculating the matrix elements for
+// Process: g g > t t~ WEIGHTED=2 @1
+// *   Decay: t > w+ b WEIGHTED=2
+// *     Decay: w+ > e+ ve WEIGHTED=2
+// *   Decay: t~ > w- b~ WEIGHTED=2
+// *     Decay: w- > e- ve~ WEIGHTED=2
+// Process: g g > t t~ WEIGHTED=2 @1
+// *   Decay: t > w+ b WEIGHTED=2
+// *     Decay: w+ > mu+ vm WEIGHTED=2
+// *   Decay: t~ > w- b~ WEIGHTED=2
+// *     Decay: w- > e- ve~ WEIGHTED=2
+// Process: g g > t t~ WEIGHTED=2 @1
+// *   Decay: t > w+ b WEIGHTED=2
+// *     Decay: w+ > e+ ve WEIGHTED=2
+// *   Decay: t~ > w- b~ WEIGHTED=2
+// *     Decay: w- > mu- vm~ WEIGHTED=2
+// Process: g g > t t~ WEIGHTED=2 @1
+// *   Decay: t > w+ b WEIGHTED=2
+// *     Decay: w+ > mu+ vm WEIGHTED=2
+// *   Decay: t~ > w- b~ WEIGHTED=2
+// *     Decay: w- > mu- vm~ WEIGHTED=2
+//--------------------------------------------------------------------------
+
+class mg5_sm_ttbar2WbWb_WW2lvlv
+{
+  public:
+
+    // Constructor.
+    mg5_sm_ttbar2WbWb_WW2lvlv()
+    {
+      for(std::size_t i = 0; i < nprocesses; ++i)
+      {
+        jamp2[i] = nullptr;
+      }
+    }
+
+    // Destructor.
+    virtual ~mg5_sm_ttbar2WbWb_WW2lvlv()
+    {
+      for(std::size_t i = 0; i < nprocesses; ++i)
+      {
+        if(jamp2[i])
+        {
+          delete [] jamp2[i];
+          jamp2[i] = nullptr;
+        }
+      }
+    }
+
+    // Initialize process.
+    virtual void
+    initProc(const std::string & param_card_name);
+
+    // Calculate flavour-independent parts of cross section.
+    virtual void
+    sigmaKin();
+
+    // Evaluate sigmaHat(sHat).
+    virtual double
+    sigmaHat();
+
+    // Info on the subprocess.
+    virtual std::string
+    name() const
+    {
+      return "g g > e+ ve b e- ve~ b~ (sm)";
+    }
+
+    const std::vector<double> &
+    getMasses() const
+    {
+      return mME;
+    }
+
+    // Get and set momenta for matrix element evaluation
+    std::vector<double *>
+    getMomenta()
+    {
+      return p;
+    }
+
+    void
+    setMomenta(std::vector<double *> & momenta)
+    {
+      p = momenta;
+    }
+
+    void
+    setInitial(int inid1,
+               int inid2)
+    {
+      id1 = inid1;
+      id2 = inid2;
+    }
+
+    // Get matrix element vector
+    const double *
+    getMatrixElements() const
+    {
+      return matrix_element;
+    }
+
+    // Constants for array limits
+    static const int ninitial = 2; 
+    static const int nexternal = 8; 
+    static const int nprocesses = 1; 
+
+  private:
+
+    // Private functions to calculate the matrix element for all subprocesses
+    // Calculate wavefunctions
+    void
+    calculate_wavefunctions(const int perm[],
+                            const int hel[]);
+
+    static const int nwavefuncs = 15;
+    std::complex<double> w[nwavefuncs][18];
+    static const int namplitudes = 3;
+    std::complex<double> amp[namplitudes];
+
+    double matrix_1_gg_ttx_t_wpb_wp_epve_tx_wmbx_wm_emvex();
+
+    // Store the matrix element value from sigmaKin
+    double matrix_element[nprocesses];
+
+    // Color flows, used when selecting color
+    double * jamp2[nprocesses];
+
+    // Pointer to the model parameters
+    Parameters_sm_ttbar2WbWb_WW2lvlv * pars;
+
+    // vector with external particle masses
+    std::vector<double> mME;
+
+    // vector with momenta (to be changed each event)
+    std::vector < double * > p;
+    // Initial particle ids
+    int id1, id2;
+};
+
+#endif  // MG5_sm_ttbar2WbWb_WW2lvlv_H
