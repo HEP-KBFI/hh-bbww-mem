d572361037b2a4d30989bd217b7e55d86e35db1f
Christian Veelken <christian.veelken@cern.ch>
2021-09-10 15:28:44 +0300

    added protection against the case that measuredParticle is a nullptr (corresponding to a "missing" particle)

--- interface/measuredParticleAuxFunctions.h
+++ interface/measuredParticleAuxFunctions.h
@@ -10,14 +10,17 @@ template <typename T>
 const T * findGenMatch(const MeasuredParticle * measuredParticle, const std::vector<T *> & genParticles)
 {
   const T * bestMatch = nullptr;
-  double dRmatch = 1.e+3;
-  for ( typename std::vector<T *>::const_iterator genParticle = genParticles.begin();
-        genParticle != genParticles.end(); ++genParticle ) {
-    double dR = deltaR(measuredParticle->eta(), measuredParticle->phi(), (*genParticle)->eta(), (*genParticle)->phi());
-    if ( dR < 0.3 && dR < dRmatch ) 
-    {
-      bestMatch = *genParticle;
-      dRmatch = dR;
+  if ( measuredParticle )
+  {
+    double dRmatch = 1.e+3;
+    for ( typename std::vector<T *>::const_iterator genParticle = genParticles.begin();
+          genParticle != genParticles.end(); ++genParticle ) {
+      double dR = deltaR(measuredParticle->eta(), measuredParticle->phi(), (*genParticle)->eta(), (*genParticle)->phi());
+      if ( dR < 0.3 && dR < dRmatch ) 
+      {
+        bestMatch = *genParticle;
+        dRmatch = dR;
+      }
     }
   }
   return bestMatch;
